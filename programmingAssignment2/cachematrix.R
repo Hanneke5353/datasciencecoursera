a9f7b8d08a4278430c8df52c70d4d2b34998cef0
Hanneke van Veldhuizen <hanneke.van.veldhuizen@ing.nl>
2015-04-25 16:03:49 +0200

    add two functions for inverse matrix

---
+++ programmingAssignment2/cachematrix.R
@@ -0,0 +1,39 @@
+## Assignment2
+## Two function to calculate the inverse of a sqaured matrix
+
+## makeCacheMatrix sets functions that can can get or set the inverse of a matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+## This function checks if inverse is already set
+## If it it it returns the set inverse
+## otherwise it will get the data an calculate the inverse
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  message("reken maar uit")
+  return(m)
+}
+       
+
+# The end
